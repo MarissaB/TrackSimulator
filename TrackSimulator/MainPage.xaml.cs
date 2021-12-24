31155bbc8509ccf7d68aae9d1b454a6f1cc2cfea
Marissa Berresford <mberresford@nvgo.com>
2021-12-24 15:55:46 -0600

    navigation

--- TrackSimulator/MainPage.xaml.cs
+++ TrackSimulator/MainPage.xaml.cs
@@ -26,6 +26,9 @@ namespace TrackSimulator
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Mapping of tags and pages used for dictating navigation
+        /// </summary>
         private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
         {
             ("home", typeof(HomePage)),
@@ -34,42 +37,66 @@ namespace TrackSimulator
 
         private void Navigation_Loaded(object sender, RoutedEventArgs e)
         {
+            // Add handler for ContentFrame navigation.
+            ContentFrame.Navigated += On_Navigated;
 
+            // Navigation doesn't load any page by default, so load home page.
+            Navigation.SelectedItem = Navigation.MenuItems[0];
         }
 
+        /// <summary>
+        /// Event triggered when user clicks a navigation menu item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void Navigation_SelectionChanged(muxc.NavigationView sender, muxc.NavigationViewSelectionChangedEventArgs args)
         {
-            if (args.IsSettingsSelected == true)
-            {
-                Navigation_Navigate("settings", args.RecommendedNavigationTransitionInfo);
-            }
-            else if (args.SelectedItemContainer != null)
+            if (args.SelectedItemContainer != null)
             {
                 string navItemTag = args.SelectedItemContainer.Tag.ToString();
                 Navigation_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
             }
         }
 
+        /// <summary>
+        /// Navigate to the selected page
+        /// </summary>
+        /// <param name="navItemTag">Tag of the selected nav menu item</param>
+        /// <param name="transitionInfo">Animation for transition</param>
         private void Navigation_Navigate(string navItemTag, Windows.UI.Xaml.Media.Animation.NavigationTransitionInfo transitionInfo)
         {
             Type _page = null;
-            if (navItemTag == "settings")
-            {
-                //_page = typeof(SettingsPage);
-            }
-            else
-            {
-                var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
-                _page = item.Page;
-            }
-            // Get the page type before navigation so you can prevent duplicate
-            // entries in the backstack.
-            var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            (string Tag, Type Page) item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+            _page = item.Page;
+
+            // Get the page type before navigation so you can prevent duplicate entries in the backstack.
+            Type preNavPageType = ContentFrame.CurrentSourcePageType;
 
             // Only navigate if the selected page isn't currently loaded.
             if (!(_page is null) && !Equals(preNavPageType, _page))
             {
                 _ = ContentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        /// <summary>
+        /// Handle the selected item and set the header after navigation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (ContentFrame.SourcePageType != null)
+            {
+                (string Tag, Type Page) item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+
+                Navigation.SelectedItem = Navigation.MenuItems
+                    .OfType<muxc.NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                Navigation.Header =
+                    ((muxc.NavigationViewItem)Navigation.SelectedItem)?.Content?.ToString();
             }
         }
     }
